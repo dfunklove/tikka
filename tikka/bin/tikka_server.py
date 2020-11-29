8ec40fcec4d87a2e4c54e91aef9002000fb86e52
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2020-11-28 22:20:51 -0600

    Read api_key from command line or config file.

--- tikka/bin/tikka_server.py
+++ tikka/bin/tikka_server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import argparse
+import configparser
+import pkg_resources
 from tikka import tikka
 
 usage = """
@@ -11,7 +13,17 @@ parser = argparse.ArgumentParser(description="Tikka Server", usage=usage)
 parser.add_argument('-c', '--certfile', help="server certificate file", required=True)
 parser.add_argument('-k', '--keyfile', help="private key file", required=True)
 parser.add_argument('-p', '--port', default=5001, type=int)
+parser.add_argument('-a', '--api-key')
 args = parser.parse_args()
 
+api_key = args.api_key
+if not api_key:
+  config_filename = pkg_resources.resource_filename(tikka.__name__, 'config.ini')
+  print(f"Loading API_KEY from file: {config_filename} ", end='')
+  config = configparser.ConfigParser()
+  config.read(config_filename)
+  api_key = config['DEFAULT']['API_KEY']
+  print(f"...done.")
+
 print(f"Starting Tikka Server on port {args.port}")
-tikka.main(args.certfile, args.keyfile, args.port)
+tikka.main(api_key, args.certfile, args.keyfile, args.port)
